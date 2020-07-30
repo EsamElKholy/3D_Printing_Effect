7dfd7ca7e6c8075e781bf02bf0f5e685c09009d0
kaisama <0ultrakai0@gmail.com>
2020-07-30 16:24:25 +0200

    mesh slicing

---
+++ Assets/Scripts/MeshSlicer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshSlicer : MonoBehaviour
+{
+    public SlicingPlane slicingPlane;
+    private new Renderer renderer;
+    private Vector4 planeEquation;
+    private Material material;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+
+        if (renderer)
+        {
+            material = renderer.sharedMaterial;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (material)
+        {
+            material.SetVector("_SlicingPlane", slicingPlane.GetEquation());
+        }
+    }
+}
---
+++ Assets/Scripts/SlicingPlane.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlicingPlane : MonoBehaviour
+{
+    private Plane slicingPlane;
+    private Vector4 equation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slicingPlane = new Plane(transform.up, transform.position);
+        equation = new Vector4(slicingPlane.normal.x, slicingPlane.normal.y, slicingPlane.normal.z, slicingPlane.distance);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        slicingPlane.SetNormalAndPosition(transform.up, transform.position);
+
+        equation.x = slicingPlane.normal.x;
+        equation.y = slicingPlane.normal.y;
+        equation.z = slicingPlane.normal.z;
+        equation.w = slicingPlane.distance;
+    }
+
+    public Vector4 GetEquation()
+    {
+        return equation;
+    }
+}
